b6dc6b6b6d6850e4e4100bbb69fc43a2738c6576
laopo001 <353272497@qq.com>
2018-05-14 15:31:29 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+var webpack = require('webpack');
+var path = require('path')
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = function (env, webpackConfig) {
+    if (env) {
+       
+    } else {
+        env = { demo: 'demo' }
+    }
+    console.log(`./src/${env.demo}/index`)
+    return {
+        //页面入口文件配置
+        entry: {
+            index: `./src/${env.demo}/index`
+        },
+        //入口文件输出配置
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: '[name].js'
+        },
+        //插件项
+        plugins: [
+            new BrowserSyncPlugin({
+                // proxy: 'localhost:80',//要代理的端口
+                host: 'localhost',
+                port: 5000,
+                server: { baseDir: ['build'] }
+            }),
+
+        ],
+        module: {
+            //加载器配置
+            rules: [
+                // {
+                //     test: /\.tsx?$/,
+                //     loader: 'ts-loader',
+                //     options: {
+                //         configFile: 'tsconfig.json'
+                //     }
+                // },
+                {
+                    test: /\.tsx?$/,
+                    use: [
+                        'cache-loader',
+                        {
+                            loader: 'thread-loader',
+                            options: {
+                                workers: require('os').cpus().length - 1,
+                            }
+                        },
+                        {
+                            loader: 'ts-loader',
+                            options: {
+                                configFile: 'tsconfig.json',
+                                happyPackMode: true,
+                                transpileOnly: true
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js'],
+            alias: {
+            }
+        },
+        externals: {
+
+        },
+        devtool: 'source-map',
+        mode: 'development',
+        performance: { hints: false }
+    };
+}
