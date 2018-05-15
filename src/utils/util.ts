980192389d7f265f6b2b0ab44e8cf34113a11abe
liaodh <liaodh@jpush.com>
2018-05-15 14:14:00 +0800

    完成demo 画一个点

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,6 +1,6 @@
 export function initShaders(gl: WebGLRenderingContext, vshader: string, fshader: string) {
     let program = createProgram(gl, vshader, fshader);
-    gl.useProgram(program)
+    
     return program;
 }
 
@@ -8,16 +8,26 @@ export function createProgram(gl: WebGLRenderingContext, vshader: string, fshade
     const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vshader);
     const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fshader);
     const program = gl.createProgram();
-    gl.attachShader(program, vshader);
-    gl.attachShader(program, fshader);
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
     gl.linkProgram(program);
     const linked = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (!linked) {
+        console.log(gl.getProgramInfoLog(program));
+        return false;
+    }
+    gl.useProgram(program)
     return program;
 }
 
 export function loadShader(gl: WebGLRenderingContext, type, source) {
     const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
     gl.compileShader(shader);
     const compiled = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (!compiled) {
+        console.log(gl.getShaderInfoLog(shader));
+        return false;
+    }
     return shader;
 }
