46c209b541b611e954d7a892d2c86a982ff462f0
laopo001 <353272497@qq.com>
2018-05-15 21:18:25 +0800

    fix

--- src/demo1/index.ts
+++ src/demo1/index.ts
@@ -3,14 +3,16 @@ import frag from './fragment.frag';
 import { initShaders } from '../utils'
 let canvas = document.getElementById('root') as HTMLCanvasElement;
 
+let vertices = new Float32Array([-0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.5, 0.0])
+
 export class Application {
     private gl: WebGLRenderingContext;
     constructor(canvas: HTMLCanvasElement) {
         let gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
         this.gl = gl;
-        if (!initShaders(gl, vert, frag)) {
-            return;
-        }
+        let program = initShaders(gl, vert, frag);
+        const a_Position = gl.getAttribLocation(program, 'position')
+        gl.vertexAttrib3f(a_Position, 0.0, -0.5, 0.0)
         gl.clearColor(0.0, 0.0, 0.0, 1.0);
         gl.clear(this.gl.COLOR_BUFFER_BIT);
         gl.drawArrays(gl.POINTS, 0, 1);
