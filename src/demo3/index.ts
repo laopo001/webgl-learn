878898f8d7c502764a9c554cdd3b6479418ff985
laopo001 <353272497@qq.com>
2018-05-18 01:22:09 +0800

    fix

--- src/demo3/index.ts
+++ src/demo3/index.ts
@@ -34,7 +34,6 @@ export class Application {
         gl.clearColor(0.0, 0.0, 0.0, 1.0);
         gl.clear(gl.COLOR_BUFFER_BIT);
         gl.drawArrays(gl.TRIANGLES, 0, 3);
-        gl.flush();
     }
     createVbo(data: Float32Array): WebGLBuffer {
         let { gl } = this;
